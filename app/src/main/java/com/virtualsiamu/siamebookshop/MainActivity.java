591d8f9ea9e14bfc5dc86ec2617fa69c03de6bdf
manonat <buddhacomic@hotmail.com>
2016-07-10 10:59:46 +0700

    9 authen succes  userpasswod g

--- app/src/main/java/com/virtualsiamu/siamebookshop/MainActivity.java
+++ app/src/main/java/com/virtualsiamu/siamebookshop/MainActivity.java
@@ -117,6 +117,11 @@ public class MainActivity extends AppCompatActivity {
                 } else if (myPasswordString.equals(truePassword)) {
                     Toast.makeText(context, "Welcome" + loginNameString +" " + loginSurnameString,
                             Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, ServiceActivity.class);
+                    intent.putExtra("Name", loginNameString);
+                    intent.putExtra("Surname", loginSurnameString);
+                    startActivity(intent);
+                    finish();
 
 
                 } else {
